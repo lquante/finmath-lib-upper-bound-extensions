2bae679fcc8fa71838590cb46ae3fb4064d2b3c0
lquante <lennart.quante@me.com>
2019-09-17 11:54:31 +0200

    increased number of subsimulations

--- src/main/java/simulationMethods/ExecuteSimulations.java
+++ src/main/java/simulationMethods/ExecuteSimulations.java
@@ -16,8 +16,8 @@ public class ExecuteSimulations {
 	static double liborPeriodLength = 0.5;
 	// monte carlo parameters
 	static int numberOfPaths = 10000;
-	static int numberOfSubsimulationsStepA = 1000;
-	static int numberOfSubsimulationsStepB = 1000;
+	static int numberOfSubsimulationsStepA = 10000;
+	static int numberOfSubsimulationsStepB = 10000;
 	// option parameters
 	static int numberOfExercisePeriods = 20;
 	static double optionPeriodLength = 1;
@@ -30,13 +30,13 @@ public class ExecuteSimulations {
 		double swapRate = 0.02;
 		// out of the money option
 		double rateShift = -0.005;
-		testSimulation.testOptionFlatCurve(rateShift, swapRate);
+		//testSimulation.testOptionFlatCurve(rateShift, swapRate);
 		// at the money option
 		rateShift = 0;
 		testSimulation.testOptionFlatCurve(rateShift, swapRate);
 		// in the money option
 		rateShift = +0.005;
-		testSimulation.testOptionFlatCurve(rateShift, swapRate);
+		//testSimulation.testOptionFlatCurve(rateShift, swapRate);
 	}
 
 }
