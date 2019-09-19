9e88ca420bfee9505119df188490de85e47b73d3
lquante <lennart.quante@me.com>
2019-09-19 13:42:53 +0200

    changed main simulation method to be called with arguments

--- src/main/java/simulationMethods/ExecuteSimulations.java
+++ src/main/java/simulationMethods/ExecuteSimulations.java
@@ -14,16 +14,21 @@ public class ExecuteSimulations {
 	static double lastTimePoint = 30;
 	static double timeDiscretizationLength = 0.5;
 	static double liborPeriodLength = 0.5;
-	// monte carlo parameters
-	static int numberOfPaths = 40000;
-	static int numberOfSubsimulationsStepA = 1000;
-	static int numberOfSubsimulationsStepB = 1000;
 	// option parameters
 	static int numberOfExercisePeriods = 20;
 	static double optionPeriodLength = 1;
 	private static Scheme subsimulationScheme= Scheme.EULER_FUNCTIONAL;
 
+	/**
+	 * @param args the monte carlo parameters as string: number of paths, number of Subsimulations A, number of subsimulations B
+	 * @throws CalculationException if some rate calculation fails
+	 */
 	public static void main(String[] args) throws CalculationException {
+	
+		int numberOfPaths = Integer.parseInt(args[0]);
+		int numberOfSubsimulationsStepA = Integer.parseInt(args[1]);
+		int numberOfSubsimulationsStepB = Integer.parseInt(args[2]);
+		
 		SimulationFactory testSimulation = new SimulationFactory(lastTimePoint, timeDiscretizationLength, liborPeriodLength,
 				numberOfPaths, numberOfSubsimulationsStepA, numberOfSubsimulationsStepB, numberOfExercisePeriods,
 				optionPeriodLength, subsimulationScheme);
